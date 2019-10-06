163ed3191f400879fc636622559a4a91365588d4
happyleavesaoc <happyleaves.tfr@gmail.com>
2019-10-06 16:43:21 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='voobly',
-    version='1.2.7',
+    version='1.2.8',
     description='Python 3 API for Voobly, the gaming platform.',
     url='https://github.com/happyleavesaoc/python-voobly/',
     license='MIT',
@@ -18,8 +18,8 @@ setup(
         'dateparser>=0.7.0',
         'lxml>=4.3.0',
         'requests>=2.20.0',
-        'requests-cache==0.4.13',
-        'tablib==0.12.1'
+        'requests-cache>=0.4.13',
+        'tablib>=0.12.1'
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
